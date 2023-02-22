50ad89348c7658a7b6dad1088e665a55b01ae27f
Pixel <kuroi413@gmail.com>
2023-02-22 14:41:23 +0900

    Update

--- js/nyangko.js
+++ js/nyangko.js
@@ -156,26 +156,36 @@ function movePrevReferrer() {
 	// 툴 
 	function createToolsHTML() {
 		const toolsMap = {
-			'VSCode': '',
-			'XCode': '',
-			'Vim': '',
-			'Figma': '',
-			'Unity': '',
-			'PHPMyAdmin': '',
-			'CVS': '',
-			'Github': '',
-			'Github Desktop': '',
-			'Cloudflare': '',
+			'VSCode': 'vscode.png',
+			'Vim': 'vim.png',
+			'XCode': 'xcode.png',
+			'Unity': 'unity.png',
+			'PHPMyAdmin': 'pma.png',
+			'Figma': 'figma.png',
+			'Adobe XD': 'adobeXD.png',
+			'CVS': 'cvs.png',
+			'Github': 'github.png',
+			'Github Desktop': 'githubDesktop.png',
+			'Cloudflare': 'cloudflare.png',
 		};
 
-		let template = `<li>{0}</li>`;
+		let template = `
+<li>
+	<div class="knowledges-item">
+		<img src="img/tools/{1}" width="14" height="14" alt="{0}" /> 
+		{0}
+	</div>
+</li>`;
 
 		let htmlItems = [];
 		Object.entries(toolsMap).map((row) => {
 			let copyTemplate = template;
 			let key = row[0]
+			let img = row[1]
 
 			copyTemplate = copyTemplate.replace('{0}', key)
+				.replace('{0}', key)
+				.replace('{1}', img)
 
 			htmlItems.push(copyTemplate)
 		});
@@ -186,10 +196,33 @@ function movePrevReferrer() {
 	function createCareerHTML() {
 		let careerMap = [
 			{
+				project: 'Web/Server, Game',
+				date: '2022.6 ~ Current',
+				title: 'Mefi.IO',
+				subTitle: `- 채굴 게임으로 채굴 시 Metacoin 또는 Metacoin 토큰을 얻을 수 있음`,
+				desc: {
+					'Host': 'metaplanet.camp',
+					'개발 환경': 'Mac, Linux',
+					'사용 툴/DB': 'VSCode, Unity, Figma, MySQL, Redis, Metacoin',
+					'개발 언어': 'PHP, C#, NodeJS, Typescript, VueJS',
+					'진행 인원': '5인(기획자 1, 개발자 3, 디자이너 1)',
+					'참여 내용': [
+						'(Mysql, Redis) DB 구조 설계',
+						'(HTML/CSS, NodeJS, VueJS) 웹페이지 일부 작업',
+						'(PHP) API 설계 및 작업',
+						'(PHP) Server 설계 및 작업',
+						'(PHP) Socket Server 설계 및 작업',
+						'(PHP) 이체 Daemon 설계 및 작업',
+						'(NodeJS, Typescript) Telegram Bot 작업',
+						'(C#) 사용자 설정 및 사용자 정보 표기, 이벤트 작업',
+					]
+				}
+			},
+			{
 				project: 'Web/Server',
 				date: '2022.4 ~ Current',
 				title: 'Mefi.IO',
-				subTitle: '탈중앙화된 금융 서비스',
+				subTitle: `- 탈중앙화된 금융 서비스 (Metacoin, Ethereum)`,
 				desc: {
 					'Host': 'mefi.io, app.mefi.io',
 					'개발 환경': 'Mac, Linux',
@@ -210,7 +243,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2021.12 ~ Current',
 				title: 'RedScan',
-				subTitle: 'Metacoin Explorer 웹사이트',
+				subTitle: `- MetaCoin Explorer 사이트
+					- MetaCoin Transaction 에 대하여 표기 및 검색`,
 				desc: {
 					'Host': 'redscan.io',
 					'개발 환경': 'Mac, Linux',
@@ -230,7 +264,8 @@ function movePrevReferrer() {
 				project: 'iOS',
 				date: '2021.8 ~ Current',
 				title: 'MetaWallet Application',
-				subTitle: '메타코인(Blockchain) 지갑 어플리케이션',
+				subTitle: `- Metacoin 지갑 어플리케이션
+					- Metacoin이 지원하는 모든 프로토콜을 지원 `,
 				desc: {
 					'Host': 'apps.apple.com/kr/app/metawallet/id1296995715',
 					'개발 환경': 'Mac, Linux',
@@ -249,7 +284,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2021.7 ~ Current',
 				title: 'Vertical Mining',
-				subTitle: '가상화폐 채굴 프로그램, 사용자의 유휴 자원을 이용하여 채굴 코인을 채굴한뒤 메타코인 기반의 토큰으로 보상해주는 서비스',
+				subTitle: `- 가상화폐 채굴 프로그램, 사용자의 유휴 자원을 이용하여 채굴 코인을 채굴한뒤 Metacoin 기반의 토큰으로 보상해주는 서비스`,
 				desc: {
 					'Host': 'verticalmining.com',
 					'개발 환경': 'Mac, Linux',
@@ -268,7 +303,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2021.3 ~ 2021.4(보류)',
 				title: 'Airdrop Camp',
-				subTitle: '가상화폐 Airdrop 관리, 다수의 사용자에게 메타코인 기반의 토큰을 배분하기 위한 서비스',
+				subTitle: `- 가상화폐 Airdrop 관리, 다수의 사용자에게 메타코인 기반의 토큰을 배분하기 위한 서비스`,
 				desc: {
 					'Host': 'airdrop.camp',
 					'개발 환경': 'Mac, Linux',
@@ -287,7 +322,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2021.1 ~ 2021.04(보류)',
 				title: 'Playpang',
-				subTitle: 'Metacoin NFT 거래 사이트',
+				subTitle: `- Metacoin NFT 거래 사이트`,
 				desc: {
 					'Host': 'playpang.com',
 					'개발 환경': 'Mac, Linux',
@@ -306,7 +341,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2020.12 ~ 2021.6',
 				title: 'Swap.Marx.financial',
-				subTitle: 'Ethereum 기반의 토큰과 Metacoin 기반의 토큰 Swap',
+				subTitle: `- Ethereum 기반의 토큰과 Metacoin 기반의 토큰 Swap`,
 				desc: {
 					'Host': 'swap.marx.financial',
 					'개발 환경': 'Mac, Linux',
@@ -325,7 +360,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2020.10 ~ Current',
 				title: 'MetaWallet Browser Extension',
-				subTitle: '사용자들이 Metacoin을 쉽게 사용하기 위해 만든 지갑, Metamask 참조',
+				subTitle: `- 사용자들이 Metacoin을 쉽게 사용하기 위해 만든 지갑, Metamask 참조`,
 				desc: {
 					'Host': 'chrome.google.com/webstore/detail/metawallet/bkklifkecemccedpkhcebagjpehhabfb',
 					'개발 환경': 'Mac, Linux',
@@ -346,7 +381,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2020.9 ~ 2021.2',
 				title: 'Macaron',
-				subTitle: '블록체인 기반 탈중앙화 금융 서비스, Crypto Coin & Token Defi 서비스',
+				subTitle: `- 블록체인 기반 탈중앙화 금융 서비스, Crypto Coin & Token Defi 서비스`,
 				desc: {
 					'Host': 'macaron.io',
 					'개발 환경': 'Mac, Linux',
@@ -363,7 +398,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2020.5 ~ 2020.12',
 				title: 'VotingMon',
-				subTitle: '(블록체인 기반 투표/설문조사 플랫폼) 투표/설문조사 결과를 BlockChacin에 기록하고, 가상화폐를 보상으로 지급',
+				subTitle: `(블록체인 기반 투표/설문조사 플랫폼) 투표/설문조사 결과를 BlockChacin에 기록하고, 가상화폐를 보상으로 지급`,
 				desc: {
 					'Host': 'votingmon.com',
 					'개발 환경': 'Mac, Linux',
@@ -381,7 +416,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2020.5 ~ 2020.12',
 				title: 'HelloChallenge ',
-				subTitle: '(소셜 마케팅 플랫폼) 광고주의 요청에 의해 만들어진 영상을 틱톡, 트위터, 페이스북, 네이버TV, 다음 TV에 공유하고 보상을 BlockChain 기반의 토큰으로  지급',
+				subTitle: `- (소셜 마케팅 플랫폼) 광고주의 요청에 의해 만들어진 영상을 틱톡, 트위터, 페이스북, 네이버TV, 다음 TV에 공유하고 보상을 BlockChain 기반의 토큰으로 지급`,
 				desc: {
 					'Host': 'hellochalange.com',
 					'개발 환경': 'Mac, Linux',
@@ -400,7 +435,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2019.6 ~ 2021.2',
 				title: 'AutosDAQ',
-				subTitle: '가상화폐 거래소',
+				subTitle: `- Metacoin Token을 이용하여 차량을 주식화
+					-  사용자간 거래하며, 거래내역은  Metacoin의 특정 프로토콜로 처리`,
 				desc: {
 					'Host': 'autosdaq.com',
 					'개발 환경': 'Linux',
@@ -421,7 +457,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2019.5 ~ 2019.10(보류)',
 				title: 'PlayBits',
-				subTitle: '게임기반 가상화폐 거래소',
+				subTitle: `- 게임기반 가상화폐 거래소
+					- 게임 내 아이템을 NFT화 또는 Token화 하여 경매 및 판매로 거래`,
 				desc: {
 					'Host': 'playbits.io',
 					'개발 환경': 'Linux',
@@ -442,7 +479,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2019.3 ~ 2020.5',
 				title: 'BitPang',
-				subTitle: '가상화폐 거래소',
+				subTitle: `- Upbit와 같은 가상 화폐 거래소
+					- 각 코인의 거래 및 이체`,
 				desc: {
 					'Host': 'bitpang.com',
 					'개발 환경': 'Linux',
@@ -464,7 +502,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.12 ~ 2019.5(보류)',
 				title: 'FilmsDAQ',
-				subTitle: '영화를 주식화 하여 사용자간 거래하며, 거래내역은 BlockChain 으로 처리',
+				subTitle: `- Metacoin Token을 이용하여 영화 및 미디어를 주식화
+					-  사용자간 거래하며, 거래내역은  Metacoin의 특정 프로토콜로 처리`,
 				desc: {
 					'Host': 'filmsdaq.com',
 					'개발 환경': 'Linux',
@@ -485,7 +524,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.12 ~ 2020.2(보류)',
 				title: 'EntersDAQ',
-				subTitle: '연예인, 음악 등을 주식화 하여 사용자간 거래하며, 거래내역은 BlockChain 으로 처리',
+				subTitle: `- Metacoin Token을 이용하여 연예인, 음악 등을 주식화
+					-  사용자간 거래하며, 거래내역은  Metacoin의 특정 프로토콜로 처리`,
 				desc: {
 					'Host': 'entersdaq.com',
 					'개발 환경': 'Linux',
@@ -506,7 +546,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.11 ~ Current',
 				title: 'POSDAQ',
-				subTitle: '정치인을 주식화 하여 사용자간 거래하며, 거래내역은 BlockChain 으로 처리',
+				subTitle: `- Metacoin Token을 이용하여 정치인을 주식화
+					-  사용자간 거래하며, 거래내역은  Metacoin의 특정 프로토콜로 처리`,
 				desc: {
 					'Host': 'posdaq.com',
 					'개발 환경': 'Linux',
@@ -527,7 +568,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.11 ~',
 				title: 'BTCNIA',
-				subTitle: '가상화폐 거래소',
+				subTitle: `- Upbit와 같은 가상 화폐 거래소
+					- 각 코인의 거래 및 이체`,
 				desc: {
 					'Host': 'btcnia.com',
 					'개발 환경': 'Linux',
@@ -549,7 +591,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.9 ~ Current',
 				title: 'IssueToken',
-				subTitle: 'MetaCoin기반의 토큰 및 NFT 발행',
+				subTitle: `- MetaCoin기반의 토큰 및 NFT 발행`,
 				desc: {
 					'Host': 'issuetoken.io',
 					'개발 환경': 'Linux',
@@ -569,7 +611,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.8 ~ 2019.1',
 				title: 'TVPang',
-				subTitle: 'TV 녹화 및 편집, 인코딩 후 계약된 웹하드에 업로드',
+				subTitle: `- 인터넷 TV 녹화 및 편집
+					- 편집 & 인코딩 후 계약된 웹하드에 업로드`,
 				desc: {
 					'Host': 'tvpang.com',
 					'개발 환경': 'Linux',
@@ -589,7 +632,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.4 ~ 2020.5',
 				title: 'Withbit',
-				subTitle: '가상 화폐 거래소',
+				subTitle: `- Upbit와 같은 가상 화폐 거래소
+					- 각 코인의 거래 및 이체`,
 				desc: {
 					'Host': 'withbit.co.kr, withbit.com',
 					'개발 환경': 'Linux',
@@ -609,9 +653,10 @@ function movePrevReferrer() {
 			},
 			{
 				project: 'Web/Server',
-				date: '2018.2 ~ Current',
+				date: '2018.3 ~ Current',
 				title: 'MetaScan',
-				subTitle: '메타코인 Explorer 웹사이트',
+				subTitle: `- Metacoin Explorer 사이트
+					- Metacoin Transaction 에 대하여 표기 및 검색`,
 				desc: {
 					'Host': 'metascan.io',
 					'개발 환경': 'Linux',
@@ -620,7 +665,7 @@ function movePrevReferrer() {
 					'진행 인원': '5인 (기획자 1, 개발자 3, 디자이너 1)',
 					'참여 내용': [
 						'(Mysql, Redis) DB 구조 설계',
-						'(HTML/CSS, NodeJS, VueJS) 반응형 웹페이지 작업',
+						'(HTML/SCSS, NodeJS, VueJS) 반응형 웹페이지 작업',
 						'(PHP) API 작업',
 						'(PHP) Server 설계 및 작업',
 						'(PHP) Metacoin transaction을 가져오는 Daemon 작업',
@@ -632,7 +677,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2018.2 ~ Current',
 				title: 'Metacoin',
-				subTitle: '세계최초의 HyperLedger Fabric 기반의 가상화폐 웹 사이트',
+				subTitle: `- 세계최초의 HyperLedger Fabric 기반의 가상화폐 웹 사이트`,
 				desc: {
 					'Host': 'metacoin.network',
 					'개발 환경': 'Linux',
@@ -651,7 +696,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2019.9 ~ Current',
 				title: 'PHP 프레임워크 (사내용)',
-				subTitle: '내부에서 사용하기 위한 Class 기반 프레임워크 제작, 라라벨 5.x 참조.',
+				subTitle: `- 사내에서 사용하기 위한 Class 기반 프레임워크 제작, 라라벨 5.x 참조.
+					- 라우터, DB 및 파일 업로드 등을 객체 지향으로 작업`,
 				desc: {
 					'Host': 'NONE',
 					'개발 환경': 'Linux',
@@ -668,7 +714,10 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2017.12 ~ 2019.4',
 				title: 'BitPet',
-				subTitle: '세계 최초의 HyperLedger Fabric 기반의 블록체인 게임',
+				subTitle: `- 세계 최초의 HyperLedger Fabric 기반의 블록체인 게임
+					- HyperLedger Fabric에 사용자의 행위 및 보유 아이템 기록
+					- 아이템 거래를 위한 경매 & 판매 시스템
+					- 아이템 조합으로 새로운 캐릭터 생성`,
 				desc: {
 					'Host': 'bitpet.co',
 					'개발 환경': 'Linux',
@@ -688,7 +737,8 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2017.7 ~ 2019.2',
 				title: 'Vertical Mining',
-				subTitle: '가상화폐 채굴 (Ethereum, Bitcoin) 시스템 모니터링 및 Diskless O/S',
+				subTitle: `- 가상화폐 채굴 (Ethereum, Bitcoin) 시스템 모니터링 및 Diskless O/S
+					- Shell script 및 Python을 이용하여 그래픽 카드의 사용량 처리`,
 				desc: {
 					'Host': 'verticalmining.com',
 					'개발 환경': 'Linux',
@@ -698,7 +748,7 @@ function movePrevReferrer() {
 					'참여 내용': [
 						'(Mysql, Redis, Sphinx) DB 일부 설계',
 						'(HTML/CSS, Jquery) 반응형 웹페이지 작업',
-						'(PHP) Server 일부 설계 및 작업',
+						'(PHP) Socket Server 설계 및 작업',
 					]
 				}
 			},
@@ -706,7 +756,10 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2015.9 ~ 2020.7',
 				title: '배달캠프/모두의 배달',
-				subTitle: '배달 대행, 배달기사 관리 및 음식 주문 앱',
+				subTitle: `- 배달 주문 및 배달기사 관리
+					- Daum 지도를 이용한 기사 위치 및 매장 관리
+					- 자체 프로그램 및  인터넷 전화를 이용한 콜관리
+					- Socket을 이용한 주방에 주문 안내 시스템`,
 				desc: {
 					'Host': 'baedal.camp, baedalcamp.com, mobae.co.kr',
 					'개발 환경': 'Linux',
@@ -716,7 +769,7 @@ function movePrevReferrer() {
 					'참여 내용': [
 						'(Mysql, Redis, Sphinx) DB 구조 일부 설계',
 						'(HTML/CSS, Jquery) 반응형 웹페이지 작업',
-						'(PHP) Server 일부 설계 및 작업',
+						'(PHP) Socket Server 일부 설계 및 작업',
 					]
 				}
 			},
@@ -724,7 +777,10 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2015.5 ~ 2020.7',
 				title: '쿡일레븐',
-				subTitle: '공유주방 주문관리 및 배달기사 관리',
+				subTitle: `- 공유 주방, 배달 주문 및 배달기사 관리
+					- Daum 지도를 이용한 기사 위치 및 매장 관리
+					- 자체 프로그램 및  인터넷 전화를 이용한 콜관리
+					- Socket을 이용한 주방에 주문 안내 시스템`,
 				desc: {
 					'Host': 'www.cook11.co.kr',
 					'개발 환경': 'Linux',
@@ -735,6 +791,24 @@ function movePrevReferrer() {
 						'(Mysql, Redis, Sphinx) DB 구조 설계',
 						'(HTML/CSS, Jquery) 반응형 웹페이지 작업',
 						'(PHP) Server 설계 및 작업',
+						'(PHP) Socket Server 일부 설계 및 작업',
+					]
+				}
+			},
+			{
+				project: 'Web/Server',
+				date: '2014.10 ~ 2014.10',
+				title: 'NEOEZ',
+				subTitle: `- 사내 사이트`,
+				desc: {
+					'Host': 'neoez.com',
+					'개발 환경': 'Linux',
+					'사용 툴/DB': 'Vim',
+					'개발 언어': 'PHP, Smarty, HTML/CSS, Javascript',
+					'진행 인원': '2인 (개발자 1 디자이너 1)',
+					'참여 내용': [
+						'(HTML/CSS, Jquery) 반응형 웹페이지 작업',
+						'(PHP) Server 설계 및 작업'
 					]
 				}
 			},
@@ -742,7 +816,7 @@ function movePrevReferrer() {
 				project: 'Web/Server, iOS',
 				date: '2014.8 ~ 2019.2',
 				title: '동탄닷컴',
-				subTitle: '동탄 지역 내 커뮤니티',
+				subTitle: `- 동탄 지역 내 커뮤니티 사이트 (매장 소개, 중고 장터, 커뮤니티)`,
 				desc: {
 					'Host': 'dongtan.com',
 					'개발 환경': 'Linux, Mac',
@@ -761,7 +835,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2013.8 ~ 2013.12',
 				title: '[외주] 웹하드',
-				subTitle: '파일노리 와 같은 웹하드 사이트',
+				subTitle: `- 파일노리 와 같은 웹하드 사이트`,
 				desc: {
 					'Host': '',
 					'개발 환경': 'Linux',
@@ -778,7 +852,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2013.3 ~ 2020.12',
 				title: 'Alleska',
-				subTitle: '웹하드의 저작권 위반 게시물 모니터링 및 저작권 보호 요청 대행',
+				subTitle: `- 웹하드의 저작권 위반 게시물 모니터링 및 저작권 보호 요청 대행`,
 				desc: {
 					'Host': 'alleska.com',
 					'개발 환경': 'Linux',
@@ -795,7 +869,7 @@ function movePrevReferrer() {
 				project: 'Web/Server',
 				date: '2013.3 ~ 유지보수',
 				title: '컨텐츠 관리 솔루션(www.neocontents.co.kr, dcms.co.kr)',
-				subTitle: '사내 프로그램으로 웹하드에 로그인한 사용자의 게시글 관리 (컨텐츠 업로드, 글쓰기, 수정, 삭제, 끌어올리기)',
+				subTitle: `- Dephi로 만든 프로그램으로 웹하드에 로그인한 사용자의 게시글 관리 (컨텐츠 업로드 글쓰기 수정 삭제 끌어올리기)`,
 				desc: {
 					'Host': 'dcms.co.kr, www.neocontents.co.kr',
 					'개발 환경': 'Linux',
@@ -825,7 +899,7 @@ function movePrevReferrer() {
 	<div class="right-part">
 		<h4 class="item-title">{2}</h4>
 		<div class="career-list">
-			<div class="career-title">{3}</div>
+			<div class="career-title pre-line">{3}</div>
 			<ul>{4}</ul>
 		</div>
 	</div>
